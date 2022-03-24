6b1aa0f8404fbdd1ce6fd9004bc12ffe9d1b821a
demon <demonsd98@outlook.com>
2022-03-24 20:54:54 +0000

    More testbenches.

---
+++ sim_1/new/Swithes_TB.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 24.03.2022 20:49:52
+// Design Name: 
+// Module Name: Swithes_TB
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Swithes_TB(
+
+    );
+endmodule
